8d77086a4be8adaee39c2bd88e35d054918c21e2
utk09 <utkarsh.tiwari@somaiya.edu>
2019-07-07 08:32:12 +0530

    added quicksort

---
+++ QuickSort/1_QuickSort.py
@@ -0,0 +1,15 @@
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+print(quicksort([10,5,3,2,11,32,1]))
+
+
+
+    
