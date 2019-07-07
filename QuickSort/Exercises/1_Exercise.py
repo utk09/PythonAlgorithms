ea6bc2f4fa2d07b1ca25ca9d5885a4d40593df3d
utk09 <utkarsh.tiwari@somaiya.edu>
2019-07-07 09:08:15 +0530

    added quicksort exercise

--- QuickSort/Exercises/1_Exercise.py
+++ QuickSort/Exercises/1_Exercise.py
@@ -3,8 +3,9 @@ user_input = str(input("Eg - 2,3,4 :: "))
 x = user_input.split(",")
 x = list(map(int, x))
 
-def recursive_sum(x):
-    for i in list(x):
-        print(i)
+def recursive_sum(list):
+        if list == []:
+                return 0
+        return list[0] + recursive_sum(list[1:]) 
 
-recursive_sum(x)
+print(recursive_sum(x))
